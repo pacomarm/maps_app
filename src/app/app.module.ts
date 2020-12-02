4eb7ef3fbe35766e5544628ef7df45c016b5af64
pacomarm <francisco.marmolejoml@udlap.mx>
2020-12-01 19:29:38 -0600

    map fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent,
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAMl87urQ8minngFCng8mLOxz-_d9Be4bU'
+    }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,8 @@
+export class Marcador {
+    constructor( public lat: number,
+                 public lng: number,
+                 public titulo = 'Sin título',
+                 public desc = 'Sin Descripción'){}
+}
+
+
